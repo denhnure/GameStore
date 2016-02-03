35222fdada9cd08329a33df5a26e041f674d9ce6
denhnure <denhnure@gmail.com>
2016-02-03 21:28:05 +0200

    Removing obsolete and installing up-to-date NuGet packages

--- packages/Ninject.Web.Common.WebHost.3.2.3.0/Content/App_Start/NinjectWebCommon.cs.pp
+++ packages/Ninject.Web.Common.WebHost.3.2.3.0/Content/App_Start/NinjectWebCommon.cs.pp
@@ -1,7 +1,7 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(GameStore.WebUI.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(GameStore.WebUI.App_Start.NinjectWebCommon), "Stop")]
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof($rootnamespace$.App_Start.NinjectWebCommon), "Stop")]
 
-namespace GameStore.WebUI.App_Start
+namespace $rootnamespace$.App_Start
 {
     using System;
     using System.Web;
