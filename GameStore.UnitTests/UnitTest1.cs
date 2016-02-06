842c6e4d1c4fd74fad32149fd8bd469eb820fa70
denhnure <denhnure@gmail.com>
2016-02-06 15:16:24 +0200

    Adding paging functionality

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Controllers;
+using GameStore.WebUI.HtmlHelpers;
+using GameStore.WebUI.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace GameStore.UnitTests
 {
@@ -7,8 +16,75 @@ namespace GameStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanPaginate()
         {
+            var mock = new Mock<IGameRepository>();
+            
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game {GameId = 1, Name = "Game1"},
+                new Game {GameId = 2, Name = "Game2"},
+                new Game {GameId = 3, Name = "Game3"},
+                new Game {GameId = 4, Name = "Game4"},
+                new Game {GameId = 5, Name = "Game5"}
+            });
+
+            var controller = new GameController(mock.Object) {pageSize = 3};
+
+            var result = (GameListViewModel) controller.List(2).Model;
+
+            var games = result.Games.ToList();
+            Assert.IsTrue(games.Count == 2);
+            Assert.AreEqual(games[0].Name, "Game4");
+            Assert.AreEqual(games[1].Name, "Game5");
+        }
+
+        [TestMethod]
+        public void CanGeneratePageLinks()
+        {
+            HtmlHelper myHelper = null;
+
+            var pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                            + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+                            + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+                result.ToString());
+        }
+
+        [TestMethod]
+        public void CanSendPaginationViewModel()
+        {
+            var mock = new Mock<IGameRepository>();
+
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game {GameId = 1, Name = "Game1"},
+                new Game {GameId = 2, Name = "Game2"},
+                new Game {GameId = 3, Name = "Game3"},
+                new Game {GameId = 4, Name = "Game4"},
+                new Game {GameId = 5, Name = "Game5"}
+            });
+
+            var controller = new GameController(mock.Object) { pageSize = 3 };
+
+            var result = (GameListViewModel)controller.List(2).Model;
+
+            PagingInfo pageInfo = result.PagingInfo;
+
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
--- GameStore.WebUI/Controllers/GameController.cs
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -1,24 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using GameStore.Domain.Abstract;
+using GameStore.WebUI.Models;
 
 namespace GameStore.WebUI.Controllers
 {
     public class GameController : Controller
     {
         private IGameRepository repository;
+        public int pageSize = 4;
 
         public GameController(IGameRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page=1)
         {
-            return View(repository.Games);
+            var model = new GameListViewModel
+            {
+                Games = repository.Games.OrderBy(game => game.GameId).Skip((page - 1)*pageSize).Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Games.Count()
+                }
+            };
+
+            return View(model);
         }
 	}
 }
